8e23b07ca2958452035e60f797c27f24aa0953f9
arminKovacs <armin0191@gmail.com>
2021-10-11 10:12:14 +0200

    Fix configuration issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './App.css'
 import Routes from './routes/Routes'
 
+import 'normalize.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
+
 function App() {
   return (
     <Routes />
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Routes = () => {
   return (
     <div>
-
+      test
     </div>
   )
 }
